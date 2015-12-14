7b31f4079eb735224fd8a6ee3f1291021e3bf759
Spencer Rohan <spencermrohan@gmail.com>
2015-12-14 14:57:51 -0600

    ADD Controllers

--- database/migrations/2015_12_14_192940_create_themes_table.php
+++ database/migrations/2015_12_14_192940_create_themes_table.php
@@ -15,7 +15,7 @@ class CreateThemesTable extends Migration
         Schema::create('themes', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('product_id')->unsigned();
-            $table->forign('product_id')
+            $table->foreign('product_id')
                   ->references('id')->on('products');
             $table->string('hex', 32);
             $table->string('color', 32);
