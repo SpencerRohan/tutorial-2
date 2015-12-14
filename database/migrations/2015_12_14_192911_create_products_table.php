862a6f478124316a91b8b5a04b14881068178ede
Spencer Rohan <spencermrohan@gmail.com>
2015-12-14 14:04:34 -0600

    ADD migrations and models

--- database/migrations/2015_12_14_192911_create_products_table.php
+++ database/migrations/2015_12_14_192911_create_products_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('code', 36)
+            $table->string('name', 36);
+            $table->string('style', 36);
+            $table->text('headline');
+            $table->text('content');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('products');
     }
 }
---
+++ database/migrations/2015_12_14_192940_create_themes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThemesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('themes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id')->unsigned();
+            $table->forign('product_id')
+                  ->references('id')->on('products');
+            $table->string('hex', 32);
+            $table->string('color', 32);
+            $table->string('headline', 32);
+            $table->string('sm-headline', 32);
+            $table->string('body', 32);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('themes');
+    }
+}
