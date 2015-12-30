3ecd620536b16f7d87b61797d80adb9099c439e1
Spencer Rohan <spencermrohan@gmail.com>
2015-12-30 09:59:53 -0600

    UPDATE Password confirmation for USER  validation

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -17,8 +17,14 @@
             <h6>{!! Form::label('Email', 'Email:') !!}</h6>
             {!! Form::text('email',null,['class'=>'form-control']) !!}
         </div>
-        <h6>{!! Form::label('Password', 'Password') !!}</h6>
-        {!! Form::text('password',null,['class'=>'form-control']) !!}
+        <div class="form-group">
+            <h6>{!! Form::label('Password', 'Password:') !!}</h6>
+            {!! Form::text('password',null,['class'=>'form-control']) !!}
+        </div>
+                <div class="form-group">
+            <h6>{!! Form::label('Confirm Password', 'Confirm Password:') !!}</h6>
+            {!! Form::text('password_confirmation',null,['class'=>'form-control']) !!}
+        </div>
         <div class="form-group">
             <h6>{!! Form::label('is_admin', 'Make Admin:') !!}
             {!! Form::Checkbox('is_admin', '1', ['class'=>'form-control']) !!}
